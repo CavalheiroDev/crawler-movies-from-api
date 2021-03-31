4eebb6e5e65c8710bf6fbbaa986edbb07cf46083
CavalheiroDev <ga3-cavalheiro@hotmail.com>
2021-03-31 15:43:19 -0300

    most popular movies finished

---
+++ mostPopularMovies.js
@@ -0,0 +1,23 @@
+import fetchHTML from './fetchHTML.js';
+import cheerio from 'cheerio';
+
+async function getMostPopularMovies() {
+  const html = await fetchHTML('https://www.imdb.com/chart/moviemeter');
+
+  const $ = cheerio.load(html);
+
+  let moviesDetails = {
+    titles: $('.titleColumn > a').map(function () {
+      return $(this).text();
+    }).get(),
+    images: $('.posterColumn').map(function () {
+      return $(this).find('img').attr('src');
+    }).get()
+  }
+  // for (let index = 0; index < moviesDetails.titles.length; index++) {
+  //   console.log(moviesDetails.titles[index], moviesDetails.images[index]);
+  // }
+  return moviesDetails;
+}
+
+getMostPopularMovies();
