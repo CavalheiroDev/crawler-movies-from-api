6088e025714e764870970f3710471452586f62df
CavalheiroDev <ga3-cavalheiro@hotmail.com>
2021-03-31 23:17:33 -0300

    Crawler most popular movies finished

---
+++ models/Movie.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let MovieSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+    required: true
+  },
+  topick: {
+    type: Boolean,
+    required: true
+  }
+});
+
+
